4ce9183a9d349bd5ed3aea2a26f241429c82e8a5
Thomas Evensen <thomeven@gmail.com>
2024-09-07 19:37:26 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ParseRsyncOutput",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ParseRsyncOutput",
+            targets: ["ParseRsyncOutput"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ParseRsyncOutput"),
+        .testTarget(
+            name: "ParseRsyncOutputTests",
+            dependencies: ["ParseRsyncOutput"]
+        ),
+    ]
+)
