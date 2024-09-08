c4b1b90fce79009575eab50309f91d3f4be605a5
Thomas Evensen <thomeven@gmail.com>
2024-09-08 16:18:17 +0200

    Update ParseRsyncOutputTests.swift

--- Tests/ParseRsyncOutputTests/ParseRsyncOutputTests.swift
+++ Tests/ParseRsyncOutputTests/ParseRsyncOutputTests.swift
@@ -5,7 +5,6 @@ import Testing
 
 @MainActor
 @Suite final class TestParseRsyncOutput {
-    let rsyncver3: Bool = false
     var parsersyncoutput: ParseRsyncOutput?
     
     var userHomeDirectoryURLPath: URL? {
@@ -18,11 +17,9 @@ import Testing
         }
     }
     
-    func readloggfile() -> [String]? {
-        let fm = FileManager.default
+    func readloggfileV3() -> [String]? {
         if let homepath = userHomeDirectoryURLPath {
             let ver3 = "GitHub/ParseRsyncOutput/TestData/ver3.txt"
-            let ver2 = "GitHub/ParseRsyncOutput/TestData/ver2.txt"
             let fileURL = homepath.appendingPathComponent(ver3)
 
             do {
@@ -41,13 +38,50 @@ import Testing
         }
         return nil
     }
+    
+    func readloggfileV2() -> [String]? {
+        if let homepath = userHomeDirectoryURLPath {
+            let ver2 = "GitHub/ParseRsyncOutput/TestData/ver2.txt"
+            let fileURL = homepath.appendingPathComponent(ver2)
 
-    @Test func executetest() {
-        
-        let array = readloggfile()
+            do {
+                let data = try Data(contentsOf: fileURL)
+                let filedata = String(data: data, encoding: .utf8)
+                var logarray = [String]()
+                if let line = filedata?.components(separatedBy: .newlines) {
+                    for i in 0 ..< line.count {
+                        logarray.append(line[i])
+                    }
+                }
+                return logarray
+            } catch  {
+                return nil
+            }
+        }
+        return nil
+    }
+
+    @Test func executetestV3() {
+        let array = readloggfileV3()
         if let array {
             let trimmedoutputfromrsync = TrimOutputFromRsync(array).trimmeddata
-            let parsersyncoutput = ParseRsyncOutput(trimmedoutputfromrsync, rsyncver3)
+            let parsersyncoutput = ParseRsyncOutput(trimmedoutputfromrsync, true)
+            print(parsersyncoutput.stats)
+            print(parsersyncoutput.numbersonly?.transferNum)
+            print(parsersyncoutput.numbersonly?.transferNumSize)
+            print(parsersyncoutput.numbersonly?.deletefiles)
+            print(parsersyncoutput.numbersonly?.newfiles)
+            print(parsersyncoutput.numbersonly?.totDir)
+            print(parsersyncoutput.numbersonly?.totNum)
+            print(parsersyncoutput.numbersonly?.totNumSize)
+        }
+    }
+    
+    @Test func executetestV2() {
+        let array = readloggfileV2()
+        if let array {
+            let trimmedoutputfromrsync = TrimOutputFromRsync(array).trimmeddata
+            let parsersyncoutput = ParseRsyncOutput(trimmedoutputfromrsync, false)
             print(parsersyncoutput.stats)
             print(parsersyncoutput.numbersonly?.transferNum)
             print(parsersyncoutput.numbersonly?.transferNumSize)
