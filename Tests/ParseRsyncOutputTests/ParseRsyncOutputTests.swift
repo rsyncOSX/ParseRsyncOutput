5622f9b82fb542ca16cb77509cbef4a86f627bb1
Thomas Evensen <thomeven@gmail.com>
2024-09-08 16:13:50 +0200

    update

--- Tests/ParseRsyncOutputTests/ParseRsyncOutputTests.swift
+++ Tests/ParseRsyncOutputTests/ParseRsyncOutputTests.swift
@@ -5,88 +5,57 @@ import Testing
 
 @MainActor
 @Suite final class TestParseRsyncOutput {
-    let rsyncver3: Bool = true
+    let rsyncver3: Bool = false
     var parsersyncoutput: ParseRsyncOutput?
+    
+    var userHomeDirectoryURLPath: URL? {
+        let pw = getpwuid(getuid())
+        if let home = pw?.pointee.pw_dir {
+            let homePath = FileManager.default.string(withFileSystemRepresentation: home, length: Int(strlen(home)))
+            return URL(fileURLWithPath: homePath)
+        } else {
+            return nil
+        }
+    }
+    
+    func readloggfile() -> [String]? {
+        let fm = FileManager.default
+        if let homepath = userHomeDirectoryURLPath {
+            let ver3 = "GitHub/ParseRsyncOutput/TestData/ver3.txt"
+            let ver2 = "GitHub/ParseRsyncOutput/TestData/ver2.txt"
+            let fileURL = homepath.appendingPathComponent(ver3)
 
-    func processtermination(outputfromrsync: [String]) {
-        let trimmedoutputfromrsync = TrimOutputFromRsync(outputfromrsync).trimmeddata
-        let parsersyncoutput = ParseRsyncOutput(trimmedoutputfromrsync, rsyncver3)
-        let result = stats(parsersyncoutput: parsersyncoutput)
-        print(result)
-        print(trimmedoutputfromrsync)
+            do {
+                let data = try Data(contentsOf: fileURL)
+                let filedata = String(data: data, encoding: .utf8)
+                var logarray = [String]()
+                if let line = filedata?.components(separatedBy: .newlines) {
+                    for i in 0 ..< line.count {
+                        logarray.append(line[i])
+                    }
+                }
+                return logarray
+            } catch  {
+                return nil
+            }
+        }
+        return nil
     }
 
     @Test func executetest() {
-        let arguments = Arguments().arguments
-        let process = RsyncProcess(arguments: arguments,
-                                   processtermination: processtermination)
-        process.executeProcess(rsyncver3)
-    }
-
-    func stats(parsersyncoutput: ParseRsyncOutput) -> String {
-        let numberOfFiles = String(parsersyncoutput.transferNum ?? 0)
-        let sizeOfFiles = String(parsersyncoutput.transferNumSize ?? 0)
-        var numbers: String?
-        var parts: [String]?
-        guard parsersyncoutput.resultRsync != nil else {
-            let size = numberOfFiles + " files :" + sizeOfFiles + " KB" + " in just a few seconds"
-            return size
-        }
-        if rsyncver3, let resultRsync = parsersyncoutput.resultRsync {
-            // ["sent", "409687", "bytes", "", "received", "5331", "bytes", "", "830036.00", "bytes/sec"]
-            let newmessage = resultRsync.replacingOccurrences(of: ",", with: "")
-            parts = newmessage.components(separatedBy: " ")
-        } else {
-            // ["sent", "262826", "bytes", "", "received", "2248", "bytes", "", "58905.33", "bytes/sec"]
-            if let resultRsync = parsersyncoutput.resultRsync {
-                parts = resultRsync.components(separatedBy: " ")
-            }
-        }
-        var bytesTotalsent: Double = 0
-        var bytesTotalreceived: Double = 0
-        var bytesTotal: Double = 0
-        var bytesSec: Double = 0
-        var seconds: Double = 0
-        guard (parts?.count ?? 0) > 9 else { return "0" }
-        // Sent and received
-        bytesTotalsent = Double(parts?[1] ?? "0") ?? 0
-        bytesTotalreceived = Double(parts?[5] ?? "0") ?? 0
-        if bytesTotalsent > bytesTotalreceived {
-            // backup task
-            // let result = resultsent! + parts![8] + " b/sec"
-            bytesSec = Double(parts?[8] ?? "0") ?? 0
-            seconds = bytesTotalsent / bytesSec
-            bytesTotal = bytesTotalsent
-        } else {
-            // restore task
-            // let result = resultreceived! + parts![8] + " b/sec"
-            bytesSec = Double(parts?[8] ?? "0") ?? 0
-            seconds = bytesTotalreceived / bytesSec
-            bytesTotal = bytesTotalreceived
-        }
-        numbers = formatresult(numberOfFiles: numberOfFiles, bytesTotal: bytesTotal, seconds: seconds)
-
-        print(numberOfFiles)
-        print(sizeOfFiles)
-        print(bytesTotalsent)
-        print(bytesTotalreceived)
-        print(bytesTotal)
-        print(bytesSec)
-        print(seconds)
-
-        return numbers ?? ""
-    }
-
-    private func formatresult(numberOfFiles: String?, bytesTotal: Double, seconds: Double) -> String {
-        // Dont have numbers of file as input
-        if numberOfFiles == nil {
-            String(parsersyncoutput?.output?.count ?? 0) + " files : " +
-                String(format: "%.2f", (bytesTotal / 1000) / 1000) +
-                " MB in " + String(format: "%.2f", seconds) + " seconds"
-        } else {
-            numberOfFiles! + " files : " +
-                String(format: "%.2f", (bytesTotal / 1000) / 1000) +
-                " MB in " + String(format: "%.2f", seconds) + " seconds"
+        
+        let array = readloggfile()
+        if let array {
+            let trimmedoutputfromrsync = TrimOutputFromRsync(array).trimmeddata
+            let parsersyncoutput = ParseRsyncOutput(trimmedoutputfromrsync, rsyncver3)
+            print(parsersyncoutput.stats)
+            print(parsersyncoutput.numbersonly?.transferNum)
+            print(parsersyncoutput.numbersonly?.transferNumSize)
+            print(parsersyncoutput.numbersonly?.deletefiles)
+            print(parsersyncoutput.numbersonly?.newfiles)
+            print(parsersyncoutput.numbersonly?.totDir)
+            print(parsersyncoutput.numbersonly?.totNum)
+            print(parsersyncoutput.numbersonly?.totNumSize)
         }
     }
 }
